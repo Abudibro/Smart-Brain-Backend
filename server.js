548855b58ce32ea345ee7fcf4476c3d7cb02b6c4
Abudibro <abudi.hijazi.01@gmail.com>
2020-06-25 13:15:36 +0100

    fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt =  require('bcrypt')
-const cors = require('cors');
+// const cors = require('cors');
 const knex = require('knex');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -19,7 +19,7 @@ const db = knex({
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 
 app.get('/', (req, res ) => {
