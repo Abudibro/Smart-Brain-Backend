ea9fd24ef2b2a204e83089360a6f185455b7065c
Abudibro <abudi.hijazi.01@gmail.com>
2020-06-25 10:09:05 +0100

    correcting port issue

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/profile/:id', profile.handleProfile(db))
 app.put('/image', image.handleImage(db))
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`app is listening to port ${process.env.PORT}`)
 })
 
